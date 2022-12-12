c848ea8ea0ced4cfe489f105c2e60d50e942e675
SoftPoachedEggs <115499632+SoftPoachedEggs@users.noreply.github.com>
2022-12-11 21:31:20 -0500

    connected code and cleaned up comments

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 const alphabetLower = ['a','b','c','d','e','f','g','j','i','j','k','l','m','n','o','p','q','r','s','t','u','b','w','x','y','z'];
 const alphabetUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const symbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
 var passwordLength = "";
+var concatArray = [];
 
 function generatePassword() {
         console.log("Hey! You clicked the button")
-        return "createdPassword";
-}
-
+  
 // Write password to the #password input
-function writePassword() {
-  var password  = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var concatString = [];
-  //var createdPassword = randomString;
-  passwordText.value = password;
-
 
   //pw length check
   var passwordLength = prompt("How many charachters would you like your password? (ie. 8-128)");
@@ -30,54 +24,61 @@ function writePassword() {
 
       console.log("The string will be: ", passwordLength ) //console pw length criteria
 
+  
+
   var passwordLower = confirm("Would you like to include lowercase letters?");
   var passwordUpper = confirm("Would you like to include uppercase letter?");
   var passwordSymbols = confirm("Would you like to include symbols in the password?");
   var passwordNumbers = confirm("would you like to include numbers in the password?");
+  
+  // while loop if no criteria is selected
+  while(passwordLower === false && passwordUpper === false && passwordSymbols === false && passwordNumbers === false){
+    alert("Please select at least one criteria for your password.");
+    var passwordLower = confirm("Would you like to include lowercase letters?");
+    var passwordUpper = confirm("Would you like to include uppercase letter?");
+    var passwordSymbols = confirm("Would you like to include symbols in the password?");
+    var passwordNumbers = confirm("would you like to include numbers in the password?");
+  }
 
+//Concat the array
   if (passwordLower) {
-    concatString = concatString.concat(alphabetLower)
+    concatArray = concatArray.concat(alphabetLower)
   }
   if (passwordUpper) {
-    concatString = concatString.concat(alphabetUpper)
+    concatArray = concatArray.concat(alphabetUpper)
   }
   if (passwordSymbols) {
-    concatString = concatString.concat(symbols)
+    concatArray = concatArray.concat(symbols)
   }
   if (passwordNumbers) {
-    concatString = concatString.concat(numbers)
+    concatArray = concatArray.concat(numbers)
   }
+  console.log("concat string: ", concatArray);
 
   
-  console.log("concat string: ", concatString);
+  var newPassword = ''
+
+  for (var i = 0; i < passwordLength; i++) {
+    newPassword = newPassword + concatArray[Math.floor(Math.random() * concatArray.length)];
+    console.log("generated pw:", newPassword)
+  }
+  return newPassword;
+
 }
 
 generateBtn.addEventListener("click", writePassword); 
 
 
-
-//Ask for criteria and then form concat array
-
-
-//concat the results of confirm criteria to one large array
-
-
-
-
-
-
-// above creates variable = concatString
-
-//create string of combined criteria array and form string randomly using for loop
-//Bellow is random string function
-
+function writePassword() {
+  var password  = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
 }
 
-/*
 
-}
 
-}
+
 
 // Write loop for errors on input. 
 
@@ -85,7 +86,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // Add event listener to generate button
 
-
+/*
 1. Prompt User on password criteria 
   A. length
   B. Lower, Upper, Symbols, Numbers
